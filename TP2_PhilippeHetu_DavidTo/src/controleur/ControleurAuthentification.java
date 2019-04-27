56e77f4b0f75f57c26fd2ce1040924d9e4f2dc47
david to <davidt_888@hotmail.com>
2019-04-27 19:02:36 -0400

    creation Controleur Authentification

---
+++ TP2_PhilippeHetu_DavidTo/src/controleur/ControleurAuthentification.java
@@ -0,0 +1,45 @@
+package controleur;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+import modeles.Authentification;
+import vues.ChoixForm;
+import vues.IdentificationUtilisateur;
+
+
+
+public class ControleurAuthentification implements ActionListener{
+	private IdentificationUtilisateur vue;
+	private JButton[] groupe;
+	private JTextField[] textes;
+
+	public ControleurAuthentification(IdentificationUtilisateur form) {
+		vue = form;
+		groupe = vue.getBtn();
+		textes = vue.getTexte();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == groupe[0]) {
+			Authentification auth = new Authentification(textes);
+			
+			if (auth.getVerif()) {
+				new ChoixForm().setVisible(true);;
+				vue.dispose();
+			} else {
+				JOptionPane.showMessageDialog(null, "Le nom ou le mot de passe entrer est invalide","Erreur",JOptionPane.ERROR_MESSAGE);
+			}
+		} else if (e.getSource() == groupe[1]) {
+			new Quitter();
+		}
+	}
+
+}
